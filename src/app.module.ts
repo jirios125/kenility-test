ada04a6bb90a5b2b83ee660a2eb4af20daff6e61
Juan Ignacio Rios <56406317+jirios125@users.noreply.github.com>
2024-01-21 13:25:12 -0300

    chore: setup mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { AuthModule } from './auth/auth.module';
 import { ProductModule } from './product/product.module';
 import { OrderModule } from './order/order.module';
 import { ManagementModule } from './management/management.module';
+import {MongooseModule} from "@nestjs/mongoose";
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-  imports: [AuthModule, ProductModule, OrderModule, ManagementModule],
+  imports: [AuthModule, ProductModule, OrderModule, ManagementModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DB.URL)],
   controllers: [],
   providers: [],
 })
