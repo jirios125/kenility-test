1f58b445d9f0a9df4d3bd533e270278746855655
Juan Ignacio Rios <56406317+jirios125@users.noreply.github.com>
2024-01-22 14:03:35 -0300

    fix: management endpoints to order module

--- src/management/management.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('management')
-export class ManagementController {}
--- src/management/management.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ManagementService } from './management.service';
-import { ManagementController } from './management.controller';
-
-@Module({
-  providers: [ManagementService],
-  controllers: [ManagementController],
-})
-export class ManagementModule {}
