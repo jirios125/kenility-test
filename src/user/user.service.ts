9f9084dbea1e79a597e15ea7e03576a6b166d18b
Juan Ignacio Rios <56406317+jirios125@users.noreply.github.com>
2024-02-02 14:38:11 -0300

    fix: import of compare and bcrypt

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,7 +7,8 @@ import {
 } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
-import { compare, bcrypt } from 'bcrypt';
+import { compare } from 'bcrypt';
+import * as bcrypt from 'bcrypt';
 import { User } from './user.schema';
 import { UserDto } from './dto/user.dto';
 import { UserResponse } from './dto/user.response';
